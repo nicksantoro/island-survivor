936c2aeedb81fb51724ef1f4e6b427817b581408
Nicholas Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-08-31 12:29:54 -0700

    latest commit

--- game1.js
+++ game1.js
@@ -1,9 +1,13 @@
-// TODO: 
-// 1. Search "pixel art island" to find images to use for the background
-// 2. Investigate fruit drawing
-// 3. Start working fishing and farming
+// TODO FUTURE FEATURES: 
+// 1. Create new artwork
+// 3. Start working on fishing
+// 4. 
 
-
+// TODO:
+// 1. Change the 'Z' key to the 'S'
+// 2. Make objects for fruit before adding any more fruit code
+// 3. Make objects for crabs before adding any more crab code
+// 4. Ensure all objects will be colliding with the canvas before allowing them to move
 
 //////// !!!!!!!!!!!!!   collision: pythagorean theorem calc   !!!!!!!!!!!!!!!!  //////////
 
@@ -36,25 +40,38 @@ let wKeyDown = false;
 let zKeyDown = false;
 let xFruit = Math.random() * innerWidth;
 let yFruit = Math.random() * innerHeight;
+let fruit1Exists = true;
+let xFruit2 = Math.random() * innerWidth;
+let yFruit2 = Math.random() * innerHeight;
 let xCrab = Math.random() * innerWidth;
 let yCrab = Math.random() * innerHeight;
 
+
 /* ----------- OBJECT ----------- */
 
-const character = {
+let character = {
     
     x: 800, // START POINT X
     y: 450, // START POINT Y
     speed: 2,
     name: "",
-    health: 100,
-    sprite: document.getElementById("characterSprite"), 
+    health: 1,
+    spriteImage: document.getElementById("characterSprite"),
+    sprite: {
+        top: 450,
+        bottom: 450 + 120,
+        left: 800,
+        right: 800 + 80
+    },
+    // width = right - left ->  width + left (x) = right
+    // height = bottom - top -> height + top (y) = bottom
+
     // Returns the speed that the character should move at on both axis (x and y) when moving diagonally
     diagonalSpeed: function( ){
         return Math.sqrt(this.speed * this.speed + this.speed * this.speed) / 2;
     },
     draw: function( ) {
-        context.drawImage(this.sprite, this.x, this.y, 80, 120)
+        context.drawImage(this.spriteImage, this.x, this.y, 80, 120);
     },
     doSomething: function( ){
         // Inside the definition of any method, you have a "this" variable defined for you
@@ -82,6 +99,18 @@ function translatePoint( x, y, direction, distance ) {
 
     return newPoint;
 }
+
+// Returns true if rectangle1 is colliding with rectangle2
+function checkCollision( rectangle1, rectangle2 ) {
+    
+    // rectangle == { left: 0, right: 10, top: 0, bottom: 10 }
+
+    return !(rectangle2.left > rectangle1.right || 
+            rectangle2.right < rectangle1.left || 
+            rectangle2.top > rectangle1.bottom ||
+            rectangle2.bottom < rectangle1.top);
+}
+ 
 
 /* ----------- START GAME FUNCTION ----------- */
 
@@ -113,9 +142,9 @@ function startGame( ){
 
    /* ----------- AUDIO ----------- */
 
-    // let ocean = new Audio();
-    // ocean.src="148283__rmutt__oceanwaves-5.wav";
-    // ocean.play();
+    let ocean = new Audio();
+    ocean.src="148283__rmutt__oceanwaves-5.wav";
+    ocean.play();
     
     // CONTEXT
 
@@ -143,64 +172,90 @@ function startGame( ){
     setInterval( function( ){
 
         // If the 'D' key is currently down
-        if(dKeyDown === true) character.x += character.speed;   
+        if(dKeyDown === true) {
+            character.x += character.speed;
+            character.sprite.left += character.speed;
+            character.sprite.right += character.speed;
+        }
         // If the 'A' key is currently down
-        if(aKeyDown === true) character.x -= character.speed;   
+        if(aKeyDown === true) {
+            character.x -= character.speed;
+            character.sprite.left -= character.speed;
+            character.sprite.right -= character.speed;
+        } 
         // If the 'W' key is currently down
-        if(wKeyDown === true) character.y -= character.speed;  
+        if(wKeyDown === true) {
+            character.y -= character.speed;
+            character.sprite.top -= character.speed;
+            character.sprite.bottom -= character.speed;
+        } 
         // If the 'Z' key is currently down
-        if(zKeyDown === true) character.y += character.speed;
+        if(zKeyDown === true) {
+            character.y += character.speed;
+            character.sprite.top += character.speed;
+            character.sprite.bottom += character.speed;
+        }
         
         context.clearRect(0, 0, canvas.width, canvas.height);
         
-        // let fruitSprite = document.getElementById("fruitSprite");
-        // for(let i = 0; i < 5; i++) {
-        //     let xFruit = Math.random() * innerWidth;
-        //     let yFruit = Math.random() * innerHeight;
-        //     context.drawImage(fruitSprite, xFruit, yFruit, 100, 100);
-        // }
-
-        // for(let i = 0; i < 5; i++) {
-        //     let fruitSprite = document.createElement("img")
-        //         fruitSprite.setAttribute("src", "banana.svg")
-        //         console.log("fruitSprite");
-                
-        //     let x = Math.random() * innerWidth;
-        //     let y = Math.random() * innerHeight;
-        //     context.drawImage(fruitSprite, xFruit, yFruit, 100, 100);
-        //     let insertionPoint = document.getElementById("mainGame")
-        //     insertionPoint
-        // }
-        
         let destination = translatePoint(xCrab, yCrab, .3, 5);
+
+        //radians
 
         xCrab = destination.x;
         yCrab = destination.y;
 
-        //radians
+        // ENEMY
+        // let crabSprite = document.getElementById("crabSprite");
+        // context.drawImage(crabSprite, xCrab, yCrab, 100, 100);
 
-        let crabSprite = document.getElementById("crabSprite");
-        context.drawImage(crabSprite, xCrab, yCrab, 100, 100);
+        let ship = document.getElementById("ship");
+        context.drawImage(ship, 260, 145, 270, 240);
 
-        let heartA = document.getElementById("heartA");
-        context.drawImage(heartA, 60, 35, 50, 50);
-
-        let heartB = document.getElementById("heartB");
-        context.drawImage(heartB, 120, 35, 50, 50);
-
-        let heartC = document.getElementById("heartC");
-        context.drawImage(heartC, 180, 35, 50, 50);
-
-        let fruitSprite = document.createElement("img");
-        fruitSprite.className = "banana";
-        context.drawImage(fruitSprite, xFruit, yFruit, 100, 100);
+        // use i to scale the x
+        for(let i = 1; i <= character.health; i++) {
+            let heartA = document.getElementById("heartA");
+            context.drawImage(heartA, 60 * i, 35, 50, 50);
+        }
+       
+        let fruitSprite2 = document.getElementById("fruitSprite");
+        context.drawImage(fruitSprite2, xFruit2, yFruit2, 100, 100);
 
         character.draw( );
 
+        // If fruit1 exists
+        if( fruit1Exists === true ){
+            let fruitSprite1 = document.getElementById("fruitSprite");
+            context.drawImage(fruitSprite1, xFruit, yFruit, 100, 100);
+
+            let isColliding = checkCollision( character.sprite, { top: yFruit, bottom: yFruit + 100, left: xFruit, right: xFruit + 100 } );
+
+            if( isColliding === true ) {
+                // Add sound effect
+                let collectSound = new Audio();
+                collectSound.src="collectSound.wav";
+                collectSound.play();
+                // Add 1 heart
+                character.health++;
+                // Mark the fruit as non-existant
+                fruit1Exists = false;
+            }
+        }
+
     }, TIME_BETWEEN_FRAMES );
 
-   
-}
+    localStorage.setItem("score", 0);
+    setInterval(function() {
+        if(localStorage.getItem("score")) {
+            localStorage.setItem("score", +localStorage.getItem("score")+1)
+            document.querySelector("#testScore").innerHTML = localStorage.getItem("score");
+        } else {
+            localStorage.setItem("score", 0);
+        }
+    
+    },1000)
+};
+
 
 /*
 
@@ -214,9 +269,7 @@ doSomethingWithCharacter( characterX, characterY, characterSpeed, characterName,
 // The above can be accomplished with this...
 
 
-
 character.doSomething( );
-
 
 
 // HEALTH BAR - HEARTS
